ff4fa184b7bacc31b627f1b89ffc0ed6f27b6984
Shubham Srivastav <112927958+ssrivastav34@users.noreply.github.com>
2025-11-26 14:44:12 +0530

    ###

--- config/gemini.js
+++ config/gemini.js
@@ -1,9 +1,9 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 const dotenv = require('dotenv');
-
+dotenv.config();
 //const apiKey = process.env.GEMINI_API_KEY;
 const apiKey = process.env.GEMINI_API_KEY ;
-
+debugger;
 if (!apiKey) {
   throw new Error('Missing Gemini API key. Please check your .env file.');
 }
